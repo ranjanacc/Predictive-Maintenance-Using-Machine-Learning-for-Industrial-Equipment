13bd9e3974f75ef0262b40bf18afa315e8a66f69
ranjanacc <96131401+ranjanacc@users.noreply.github.com>
2024-05-03 19:00:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from prediction import predict
 st.title('PdM Web App') 
 st.markdown('To predict machine health instantly.')
 air = st.number_input('Air Temperature [K]:') 
-process = st.number_input('Process Temperature: [K]') 
-rotation = st.number_input('Rotational Speed: [rpm]') 
-torque = st.number_input('Torque: [Nm]') 
-tool_wear = st.number_input('Tool Wear: [min]') 
+process = st.number_input('Process Temperature [K]:') 
+rotation = st.number_input('Rotational Speed[rpm]:') 
+torque = st.number_input('Torque [Nm]:') 
+tool_wear = st.number_input('Tool Wear [min]:') 
 result = 10
 if st.button("Predict"):
 	result = predict(np.array([[air, process, rotation, torque, tool_wear]]))
